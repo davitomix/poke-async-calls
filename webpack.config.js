844436c4578c519a0d4292032d9fd4a5dff6c3fa
David Elì <davidelimg1@gmail.com>
2020-08-27 10:44:43 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+/* eslint-enable global-require */
