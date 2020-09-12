770505eb2b1be504be288e5d4e5273ffd898e699
David Elì <davidelimg1@gmail.com>
2020-09-12 16:32:05 -0500

    Added get abilities of pokemons

--- src/fetchAsync/pokeApi.js
+++ src/fetchAsync/pokeApi.js
@@ -2,27 +2,35 @@ import { handleResponse, handleError } from './apiUtils';
 const baseUrl = 'https://pokeapi.co/api/v2/';
 
 const getPokemonsArray = () => {
-  const max = 200, min = 1;
+  const max = 200; const min = 1;
   const pokeIdArray = [];
   let randId;
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 16; i += 1) {
     randId = Math.floor(Math.random() * (max - 1)) + min;
     pokeIdArray.push(randId);
   }
-  return pokeIdArray;
-}
+  return pokeIdArray.filter((a, b) => pokeIdArray.indexOf(a) === b);
+};
 
-export const getPokemon = async (pokemon) => {
+export const getPokemon = async pokemon => {
   try {
-    const response = await fetch(baseUrl + `pokemon/${pokemon}`);
+    const response = await fetch(`${baseUrl}pokemon/${pokemon}`);
     return handleResponse(response);
   } catch (error) {
     return handleError(error);
   }
 };
 
-const getPokemons = async () => {
-  return Promise.all(getPokemonsArray().map(pokeId => getPokemon(pokeId)));
+export const getPokemonAbility = async pokemon => {
+  try {
+    const response = await fetch(`${baseUrl}ability/${pokemon}`);
+    return handleResponse(response);
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
-export default getPokemons;
+const pokeIdArray = getPokemonsArray();
+export const getPokemons = async () => Promise.all(pokeIdArray.map(pokeId => getPokemon(pokeId)));
+export const getPokemonsAbilities = async () => Promise.all(pokeIdArray.map(pokeId => getPokemonAbility(pokeId)));
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import getPokemons from "./fetchAsync/pokeApi";
-import { getPokemon } from './fetchAsync/pokeApi'
+import { getPokemon, getPokemons, getPokemonsAbilities } from './fetchAsync/pokeApi'
 
-getPokemon('132').then(poke => console.log(poke.name));
-getPokemons().then(pokemons => pokemons.map(pokemon => console.log(pokemon.name)));
+// getPokemon('132').then(poke => console.log(poke.name));
+getPokemons().then(pokemons => pokemons.map(pokemon => console.log(pokemon.id)));
+getPokemonsAbilities().then(pokemons => pokemons.map(pokemon => console.log(pokemon)));
 
 
